95a2ab6cf73f29fe6528cab1a0768d39bd252595
madderlake <mary@maryahayne.com>
2024-03-06 12:08:01 -0800

    add more charts

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,27 +1,39 @@
 'use client';
 import Chart from '@/components/Chart';
-import { BarList, Card, Flex, Grid, Metric, Text, Title } from '@tremor/react';
+import {
+  BarList,
+  BarChart,
+  DonutChart,
+  Legend,
+  Card,
+  Flex,
+  Grid,
+  Metric,
+  Text,
+  Title,
+} from '@tremor/react';
+import { SyntheticEvent, useState } from 'react';
 const website = [
-  { name: '/home', value: 1230 },
-  { name: '/contact', value: 751 },
-  { name: '/gallery', value: 471 },
-  { name: '/august-discount-offer', value: 280 },
-  { name: '/case-studies', value: 78 },
+  { name: 'home', value: 1230 },
+  { name: 'contact', value: 751 },
+  { name: 'gallery', value: 471 },
+  { name: 'aug-promo', value: 280 },
+  { name: 'case-studies', value: 78 },
 ];
 
 const shop = [
-  { name: '/home', value: 453 },
-  { name: '/imprint', value: 351 },
-  { name: '/shop', value: 271 },
-  { name: '/pricing', value: 191 },
+  { name: 'home', value: 453 },
+  { name: 'imprint', value: 351 },
+  { name: 'shop', value: 271 },
+  { name: 'pricing', value: 191 },
 ];
 
 const app = [
-  { name: '/shop', value: 789 },
-  { name: '/product-features', value: 676 },
-  { name: '/about', value: 564 },
-  { name: '/login', value: 234 },
-  { name: '/downloads', value: 191 },
+  { name: 'shop', value: 789 },
+  { name: 'prod-features', value: 676 },
+  { name: 'about', value: 564 },
+  { name: 'login', value: 234 },
+  { name: 'downloads', value: 191 },
 ];
 
 const data = [
@@ -42,11 +54,35 @@ const data = [
   },
 ];
 
+const dataCategories = data.map((item) => item.category);
+// const dataCategories = Object.values(data.map((item) => item.data));
+
 export default function Analytics() {
+  const views = ['Donut', 'Bar List', 'Bar'];
+
+  const [view, setView] = useState('Donut');
+  const handleSelect = (e: SyntheticEvent) => {
+    setView(e.currentTarget.innerHTML);
+  };
+  const numFormatter = (number: number) =>
+    Intl.NumberFormat('us').format(number).toString();
+
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <div className="p-4 mb-4 flex bg-slate-100">
+        <ul className="flex justify-between w-1/4 text-tremor-default">
+          {views.map((view, i) => (
+            <li
+              key={i}
+              className="cursor-pointer"
+              onClick={(e) => handleSelect(e)}>
+              {view}
+            </li>
+          ))}
+        </ul>
+      </div>
       <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
-        {data.map((item) => (
+        {data.map((item, i) => (
           <Card key={item.category}>
             <Title>{item.category}</Title>
             <Flex
@@ -60,17 +96,45 @@ export default function Analytics() {
               <Text>Pages</Text>
               <Text className="text-right">Views</Text>
             </Flex>
-            <BarList
-              data={item.data}
-              valueFormatter={(number: number) =>
-                Intl.NumberFormat('us').format(number).toString()
-              }
-              className="mt-2"
-            />
+            {view === 'Donut' && (
+              <>
+                <DonutChart
+                  data={item.data}
+                  valueFormatter={numFormatter}
+                  className="mt-2"
+                />
+                <Legend
+                  categories={Object.values(item.data.map((page) => page.name))}
+                  colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
+                  className="max-w-xs mt-6 justify-center"
+                />
+              </>
+            )}
+            {view === 'Bar List' && (
+              <>
+                <BarList
+                  data={item.data}
+                  valueFormatter={numFormatter}
+                  className="mt-2"
+                />
+              </>
+            )}
+            {view === 'Bar' && (
+              <>
+                <BarChart
+                  data={item.data}
+                  valueFormatter={numFormatter}
+                  index={'name'}
+                  categories={['value']}
+                  layout="vertical"
+                  // categories={Object.values(item.data.map((page) => page.name))}
+                />
+              </>
+            )}
           </Card>
         ))}
       </Grid>
-      <Chart />
+      {/* <Chart /> */}
     </main>
   );
 }
